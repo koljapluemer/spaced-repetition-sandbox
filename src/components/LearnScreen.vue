6e847a5223f14eba80a9d2c4af82318c2968c77a
blackbrokkoli <kastomat@gmail.com>
2023-07-24 19:50:53 +0200

    green the test

--- src/components/LearnScreen.vue
+++ src/components/LearnScreen.vue
@@ -1,16 +1,24 @@
 <template>
   Welcome!
-  <span class="due-cards">{{ cards.length }} cards due.</span>
+  <span class="due-cards">{{ dueCards.length }} cards due.</span>
 
-  <Flashcard :card="props.cards[0]" />
+  <Flashcard :card="dueCards[0]" />
 </template>
 
 <script setup lang="js">
 import Flashcard from '@/components/Flashcard.vue'
-import { defineProps } from 'vue'
+import { defineProps, computed } from 'vue'
 
 const props = defineProps({
   cards: Array,
+})
+
+// computed property with only due cards
+// filter by dueAt being either in the past or null
+const dueCards = computed(() => {
+  return props.cards.filter(card => {
+    return !card.dueAt || card.dueAt <= new Date()
+  })
 })
 
 
