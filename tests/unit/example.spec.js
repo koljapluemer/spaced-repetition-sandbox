a89a6bde0d48a09d9cf7ea1bf1293ded928eb1bc
blackbrokkoli <kastomat@gmail.com>
2023-07-24 19:25:34 +0200

    remove obsolete test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,16 +1,5 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 import Flashcard from '@/components/Flashcard.vue'
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
 
 // test whether Flashcard displays the passed in prob card.front correctly within .front
 describe('Flashcard.vue', () => {
