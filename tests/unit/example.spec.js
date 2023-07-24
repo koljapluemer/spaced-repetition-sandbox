3243b353d0685b1b51fbc4da2addc5d41f50ca34
blackbrokkoli <kastomat@gmail.com>
2023-07-24 19:48:55 +0200

    add test for modified counter

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,6 +2,30 @@ import { shallowMount } from '@vue/test-utils'
 import Flashcard from '@/components/Flashcard.vue'
 import LearnScreen from '@/components/LearnScreen.vue'
 
+const exampleCards = [
+  {
+    front: "Eswatini",
+    back: "Mbabane",
+    dueAt: null,
+    interval: 1,
+    repetitions: []
+  },
+  {
+    front: "Eritrea",
+    back: "Asmara",
+    dueAt: null,
+    interval: 1,
+    repetitions: []
+  },
+  {
+    front: "Ethiopia",
+    back: "Addis Ababa",
+    dueAt: null,
+    interval: 1,
+    repetitions: []
+  },
+]
+
 // test whether Flashcard displays the passed in prob card.front correctly within .front
 describe('Flashcard.vue', () => {
   it('displays the passed in prob card.front correctly within .front', () => {
@@ -21,9 +45,28 @@ describe('LearnScreen.vue', () => {
   it('displays the text "3 cards due"', () => {
     const wrapper = shallowMount(LearnScreen, {
       props: {
-        cards: [{}, {}, {}]
+        cards: exampleCards
       }
     })
     expect(wrapper.find('.due-cards').text()).toBe('3 cards due.')
+  })
+})
+
+// check for "2 cards due." when we edit the exampleCards array so that one cards dueAt is in the future
+describe('LearnScreen.vue', () => {
+  it('displays the text "2 cards due" when one card is set in the future', () => {
+    // first, clone array so we don't mutate the original
+    const cards = JSON.parse(JSON.stringify(exampleCards))
+
+    // set the dueAt of the first card to a future date
+    cards[0].dueAt = new Date(Date.now() + 1000 * 60 * 60 * 24)
+
+    const wrapper = shallowMount(LearnScreen, {
+      props: {
+        cards: cards
+      }
+
+    })
+    expect(wrapper.find('.due-cards').text()).toBe('2 cards due.')
   })
 })
